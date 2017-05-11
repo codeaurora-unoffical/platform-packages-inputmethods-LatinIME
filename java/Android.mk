4674ff22ce29453694e2c25ef90bfb326bd350f4
Lalit Kansara <lkansara@codeaurora.org>
2017-05-11 15:16:59 +0530

    Hack: OPR1.170507.001: Keyboard crash due to LOCAL_USE_AAPT2
    LOCAL_USE_AAPT2 has been removed to fix keyboard crash
    in OPR1.170507.001 tag.
    
    Change-Id: Ic28bb982ae17b62a00f0109336d4fd9516a530e7

--- java/Android.mk
+++ java/Android.mk
@@ -31,8 +31,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_STATIC_ANDROID_LIBRARIES := \
         android-support-v4
 
-LOCAL_USE_AAPT2 := true
-
 # Do not compress dictionary files to mmap dict data runtime
 LOCAL_AAPT_FLAGS := -0 .dict
 
